9a21e44b5a4f2ec2708fa1395c0761ec99c03845
lynirl <chisanayagi@gmail.com>
2025-11-19 14:45:44 +0100

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+//le script
+
+const btn = view.btnStart;
+
+function cramptes(){
+    view.textArea.innerHTML = "haha cramptés ^^"
+}
+
+btn.addEventListener('click', cramptes);
---
+++ view.js
@@ -0,0 +1,7 @@
+//on récup les éléments de la page
+
+const view = {
+    //bouton de démarrage
+    btnStart: document.getElementById("start-btn"),
+    textArea: document.getElementById("texte")
+}
